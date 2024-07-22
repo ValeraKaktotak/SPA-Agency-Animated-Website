20e126dc0f4aaa89c9722575e4d0728fe07540cf
Valera <valerakaktotak@gmail.com>
2024-07-22 17:35:51 +0300

    added google font: Sen, added tw styles and assets images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      Sen: 'Sen'
+    },
+    container: {
+      center: true,
+      padding: '4rem'
+    },
+    extend: {
+      colors: {
+        dark_primary: '#22063f',
+        bg_light_primary: '#f8f4ff',
+        gray: '#b7c5d3'
+      },
+      backgroundImage: {
+        primaryLinear: 'linear-gradient(135deg, #bf8bec 0%, #ae95e2 100%)'
+      },
+      dropShadow: {
+        primary: '-5px 35px 40px rgba(223, 229, 236, 0.9)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-

4bf30ec0f18257e5e41c0880f041a6eea476de14
Valera <valerakaktotak@gmail.com>
2024-07-24 13:40:43 +0300

    done Hero section

---
+++ src/Content.ts
@@ -0,0 +1,22 @@
+//Import Images
+import Hero_person from '@/assets/images/Hero/person.png'
+
+export const content = {
+  hero: {
+    title: 'Luxurious Beauty',
+    paragraph:
+      'Our relaxing boutique studio is located in Georgetown Newcastle and has ample parking easy access. We pride ourselves on treating our client;s individual needs.',
+    btnText: 'BOOK NOW',
+    image: Hero_person,
+    hero_content: [
+      {
+        count: '20+',
+        text: 'Years of Experience in Beautician industries'
+      },
+      {
+        count: '230+',
+        text: 'Happy Customers'
+      }
+    ]
+  }
+}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,58 @@
 import type { FC } from 'react'
 
-interface IHero {}
+//Content
+import { content } from '@/Content'
 
-const Hero: FC<IHero> = ({}) => {
-  return <div>Hero</div>
+const Hero: FC = () => {
+  const { hero } = content
+  return (
+    <section id='home'>
+      <div className='relative flex min-h-screen flex-col-reverse items-center justify-center px-2 md:flex-row md:items-end'>
+        {/* Background block BG */}
+        <div
+          data-aos='slide-left'
+          data-aos-delay='1200'
+          className='absolute bottom-0 right-0 top-0 -z-10 w-full bg-primaryLinear'
+        ></div>
+
+        {/* First Column */}
+        <div data-aos='fade-down' className='mr-0 pb-72 pt-5 md:mr-16'>
+          <h2>{hero.title}</h2>
+          <p className='mt-5 flex max-w-[500px] justify-end'>
+            {hero.paragraph}
+          </p>
+
+          <div className='flex justify-start'>
+            <button className='btn mt-3'>{hero.btnText}</button>
+          </div>
+
+          {/* Data Section */}
+          <div className='mt-10 flex flex-col gap-10'>
+            {hero.hero_content.map((content, i) => (
+              <div
+                data-aos='fade-down'
+                data-aos-delay={i * 300}
+                key={i}
+                className={`flex max-w-80 items-center gap-5 ${i === 1 && 'flex-row-reverse text-right'}`}
+              >
+                <h3 className='text-[66px] font-bold'>{content.count}</h3>
+                <p>{content.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Image Banner Section */}
+        <div className='h-96 md:h-[55rem]'>
+          <img
+            data-aos='slide-down'
+            className='h-full object-cover'
+            src={hero.image}
+            alt='women'
+          />
+        </div>
+      </div>
+    </section>
+  )
 }
 export default Hero
